65dca94328df4df295822185459a7f0122c27300
botbugus <jebolwa6@gmail.com>
2026-07-19 16:51:27 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,4 +1,3 @@
-<?php
-$bot_token = "7827063928:AAFBL3-fM265t7e3UhO7yrVGo_WLd2Xykmc";
-$chat_id = "6621286522";
-?>
+// Konfigurasi Telegram Bot
+const BOT_TOKEN = "7827063928:AAFBL3-fM265t7e3UhO7yrVGo_WLd2Xykmc";
+const CHAT_ID = "6621286522";
